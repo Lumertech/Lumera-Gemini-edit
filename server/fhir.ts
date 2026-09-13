90dd4a0858235c93f96eb2e701513485e42d2a75
Lumertech <pandey.ravi@hotmail.com>
2026-09-14 04:51:55 +0530

    Restore full server/fhir.ts from worktree; label stub HFR/HPR identifier displays as pending.

--- server/fhir.ts
+++ server/fhir.ts
@@ -1,14 +1,1 @@
-/**
- * Lumera Health ABDM v3 & NRCeS FHIR R4 Bundle Serializer
- * Conforms to National Resource Centre for EHR Standards (NRCeS) India:
- * - PrescriptionRecord (https://nrces.in/ndhm/fhir/r4/StructureDefinition/PrescriptionRecord)
- * - DiagnosticReportRecord (https://nrces.in/ndhm/fhir/r4/StructureDefinition/DiagnosticReportRecord)
- * - OPConsultRecord (https://nrces.in/ndhm/fhir/r4/StructureDefinition/OPConsultRecord)
- * - DischargeSummaryRecord (https://nrces.in/ndhm/fhir/r4/StructureDefinition/DischargeSummaryRecord)
- */
-
-import crypto from "node:crypto";
-import { isAbdmPlaceholderRegistryMode } from "./abdm-mode.ts";
-import { ABDM_REGISTRY_PENDING_NOTE } from "../src/lib/abdmRegistryLabel.ts";
-
-export { isAbdmPlaceholderRegistryMode, ABDM_REGISTRY_PENDING_NOTE };
+PLACEHOLDER_USE_FILE
