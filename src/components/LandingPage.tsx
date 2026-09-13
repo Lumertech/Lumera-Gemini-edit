18536a01e1bb8a6de17956d1fa8b33c7f8877250
Lumertech <pandey.ravi@hotmail.com>
2026-09-13 17:07:13 +0530

    Restore LandingPage.tsx with government data request policy links.

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -33,3 +33,8 @@ import {
   ShieldAlert,
   Award
 } from "lucide-react";
+import { Link } from "react-router-dom";
+import { LEGAL_ENTITY_NAME, PRODUCT_NAME } from "../brand";
+import { BrandMark } from "./BrandMark";
+import { useNav } from "../nav/NavigationContext";
+import { surfaceToPath } from "../nav/surfaces";
